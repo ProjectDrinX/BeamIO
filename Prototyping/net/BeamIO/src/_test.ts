186a24d8a1f03d3a7dff760dbb7b4fcdc1967d58
Mathieu Colmon <mathieu@usp-3.fr>
2022-03-04 21:53:36 +0100

    Temp benchmark

---
+++ Prototyping/net/BeamIO/src/_test.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-new */
+import CompiledScheme, { Type } from './CompiledScheme';
+
+{
+  const n = 1000000;
+  const d1 = Date.now();
+  for (let i = 0; i <= n; i += 1) {
+    new CompiledScheme('flat', {
+      name: Type.String,
+      count: Type.Number,
+      isUser: Type.Boolean,
+    });
+  }
+  const d2 = Date.now();
+  console.log('Time for compile flat scheme:', (d2 - d1) / n, 'ms');
+}
+
+{
+  const n = 1000000;
+  const d1 = Date.now();
+  for (let i = 0; i <= n; i += 1) {
+    new CompiledScheme('deep', {
+      name: Type.String,
+      count: Type.Number,
+      isUser: Type.Boolean,
+      obj: {
+        name: Type.String,
+        count: Type.Number,
+        isUser: Type.Boolean,
+        obj: {
+          name: Type.String,
+          count: Type.Number,
+          isUser: Type.Boolean,
+        },
+      },
+    });
+  }
+  const d2 = Date.now();
+  console.log('Time for compile deep scheme:', (d2 - d1) / n, 'ms');
+}
