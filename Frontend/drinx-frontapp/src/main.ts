43ff8c3d29041236dab29c15c7cc03cdba643ca3
Antoine Seguin <contact@antoine-seguin.fr>
2022-03-06 20:31:06 +0100

    Add README for project root. New init for Vue app.

--- Frontend/drinx-frontapp/src/main.ts
+++ Frontend/drinx-frontapp/src/main.ts
@@ -1,6 +1,9 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
