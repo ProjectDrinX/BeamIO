2182228ea9f10330844fe49a87345f3a74801f29
Mathieu Colmon <mathieu@usp-3.fr>
2022-03-16 18:56:09 +0100

    Better 'WebClient' example look
    - Textbox, chat and userlist CSS

--- examples/web-client/src/components/UserList.vue
+++ examples/web-client/src/components/UserList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Users } from '../App.vue';
+import UserIcon from './icons/user.vue';
 
 defineProps<{
   users: Users,
@@ -8,14 +9,22 @@ defineProps<{
 
 <template>
   <div class="users">
-    <div class="item" v-for="user in users" v-bind:style="{ color: `rgb(${user.color.r}, ${user.color.g}, ${user.color.b})` }">
-      {{ user.username }}
+    <div class="item" v-for="user in users">
+      <UserIcon/>
+      <div v-bind:style="{ color: `rgb(${user.color.r}, ${user.color.g}, ${user.color.b})` }">
+        {{ user.username }}
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.users {
-
+.item {
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  column-gap: 10px;
+  background-color: #ffffff04;
+  margin-top: 1px;
 }
 </style>
--- examples/web-client/src/views/Chat.vue
+++ examples/web-client/src/views/Chat.vue
@@ -12,16 +12,16 @@ defineProps<{
 
 <template>
   <div class="topbar">
-    Connected as
+    <div>Connected as</div>
     <div v-bind:style="{
       color: `rgb(${user.color.r}, ${user.color.g}, ${user.color.b})`
     }">{{ user.username }}</div>
   </div>
   <UserList class="sidebar" :users="users"/>
   <div class="container">
-    <div class="messages">
+    <div class="messages" ref="messages">
       <div v-for="(msg, i) in messages">
-        <div v-if="!messages[i - 1] || messages[i - 1].sender !== msg.sender" v-bind:style="{
+        <div class="username" v-if="!messages[i - 1] || messages[i - 1].sender !== msg.sender" v-bind:style="{
           color: users[msg.sender]
             ? `rgb(${users[msg.sender].color.r}, ${users[msg.sender].color.g}, ${users[msg.sender].color.b})`
             : '#888',
@@ -32,11 +32,10 @@ defineProps<{
         <div class="message">{{ msg.message }}</div>
       </div>
     </div>
-    <div class="textbox">
-      <form @submit="sendMessage">
-        <input type="text" v-model="message" placeholder="Message">
-      </form>
-    </div>
+
+    <form class="textbox" @submit="sendMessage">
+      <input type="text" v-model="message" placeholder="Message">
+    </form>
   </div>
 </template>
 
@@ -48,11 +47,41 @@ export default {
     message: string,
   },
 
+  mounted() {
+    this.scrollDown(true);
+  },
+
   methods: {
-    sendMessage() {
+    scrollDown(force = false) {
+      // @ts-ignore
+      const container: HTMLElement = this.$refs.messages;
+
+      if (
+        !force
+        && (container.scrollHeight - container.scrollTop - container.clientHeight) > 10
+      ) return;
+
+      setTimeout(() => {
+        container.scrollTo(0, 10 ** 10);
+      }, 0);
+    },
+
+    sendMessage(e: Event) {
+      e.preventDefault();
+      if (!this.message) return;
+
       socket.emit('sendMessage', {
         message: this.message,
       } as typeof Schemes.sendMessage);
+
+      this.message = '';
+      this.scrollDown(true);
+    },
+  },
+
+  watch: {
+    ['messages.length']() {
+      this.scrollDown();
     },
   },
 }
@@ -65,7 +94,14 @@ export default {
   left: 0;
   right: 0;
   height: 70px;
-  background-color: beige;
+  background-color: #ffffff14;
+
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  column-gap: 6px;
+
+  z-index: 10;
 }
 
 .sidebar {
@@ -74,11 +110,22 @@ export default {
   bottom: 0;
   left: 0;
   width: 400px;
+  background-color: #ffffff08;
 }
 
 .container {
-  margin-top: 70px;
-  margin-left: 400px;
+  position: fixed;
+  top: 70px;
+  bottom: 50px;
+  left: 400px;
+  right: 0;
+}
+
+.messages {
+  padding: 10px 20px 20px;
+  overflow: hidden;
+  overflow-y: scroll;
+  height: 100%;
 }
 
 .message {
@@ -86,10 +133,22 @@ export default {
   font-size: 23px;
 }
 
+.username {
+  margin-top: 10px;
+}
+
 .textbox {
   position: fixed;
   left: 400px;
   right: 0;
   bottom: 0;
+  height: 50px;
+}
+
+.textbox > input {
+  height: 100%;
+  width: 100%;
+  padding: 10px 20px;
+  background-color: #ffffff1c;
 }
 </style>
